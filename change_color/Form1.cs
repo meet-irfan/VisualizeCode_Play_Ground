b87fd2e631080acda624067ff6feb07693a42c13
meet-irfan <irfanali.bscsf21kdk@iba-suk.edu.pk>
2024-09-10 16:23:06 +0500

    Class-task

---
+++ change_color/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Drawing; // For Color
+using System.Windows.Forms;
+
+
+namespace change_color
+{
+    public partial class Form1 : Form
+    {
+        
+    }
+}
+
+   
