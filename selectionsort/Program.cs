ac224ff8fb96f1608e72296b5b4df02321bfd66b
meet-irfan <irfanali.bscsf21kdk@iba-suk.edu.pk>
2024-10-19 00:42:17 +0500

    Selection_Sort program

---
+++ selectionsort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selectionsort
+{
+
+
+    class SelectionSortOptimized
+    {
+        static void SelectionSort(int[] arr)
+        {
+            for (int i = 0, min; i < arr.Length - 1; i++)
+            {
+                min = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                    if (arr[j] < arr[min]) min = j;
+                (arr[i], arr[min]) = (arr[min], arr[i]);
+            }
+        }
+
+        static void Main()
+        {
+            int[] arr = { 64, 25, 12, 22, 11 };
+            SelectionSort(arr);
+            Console.WriteLine(string.Join(" ", arr));
+            Console.ReadLine();
+        }
+    }
+
+
+
+
+}
